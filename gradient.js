fd6864518ae9aaebc31dcc121f2e2a568b2730c4
sowmi0518 <sowmiyavkr18@gmail.com>
2025-03-17 16:47:38 +0530

    Add files via upload

---
+++ gradient.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector("#side-navbar");
+      function show(){
+        nav.style.left = "0";
+      }
+      function closeone(){
+        nav.style.left = "-100%";
+      }
